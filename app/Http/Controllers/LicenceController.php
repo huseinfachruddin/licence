428d5e564568793335158f4f02029f6887680871
unknown <husein.18@students.amikom.ac.id>
2021-11-03 20:17:29 +0700

    husin

--- app/Http/Controllers/LicenceController.php
+++ app/Http/Controllers/LicenceController.php
@@ -82,7 +82,7 @@ class LicenceController extends Controller
         $data->user_id = $request->user_id;
         $data->licence = time();
         $data->dns = $request->dns;
-        $data->due = $request->due;
+        $data->due = date('Y-m-d',strtostr($request->due));
         $data->save();
 
         $response = [
